b2b3a796fa48e6d27e13036840b1ed988b01cd53
pdrogfer <pedro.gf527@gmail.com>
2017-01-19 11:54:17 +0100

    update

--- Examples/Constructors/Constructors.cpp
+++ Examples/Constructors/Constructors.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 using namespace std;
 
-class Constructors {
+class Thing {
+	public:
+	Thing() : thisone(12){};
+	int getThisone() { return thisone; }
+	private:
+	int thisone;
 };
 int main() {
+	Thing th;
+	cout << th.getThisone() << endl;
 	cout << "Hello there" << endl;
 	return 0;
 }
-
